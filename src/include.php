0b3a4950b9fac86899e43bab4c30768bd924ae1f
Andrew Crites <explosion-pills@aysites.com>
2012-02-23 17:49:52 -0500

    Removing unused EOF token class and allowing non-autoload include

--- src/include.php
+++ src/include.php
@@ -1,17 +1,34 @@
 <?php
-function __csspurify_autoload($file) {
-   if (file_exists(dirname(__FILE__) . "/$file.php")) {
-      include dirname(__FILE__) . "/$file.php";
+if (function_exists('spl_autoload_register')) {
+   function __csspurify_autoload($file) {
+      $path = dirname(__FILE__) . '/';
+      if (file_exists("$path$file.php")) {
+         include "$path$file.php";
+      }
+      else if (file_exists($path . "gen/$file.php")) {
+         include $path . "gen/$file.php";
+      }
+      else if (file_exists($path . "tree/$file.php")) {
+         include $path . "tree/$file.php";
+      }
+      else if (file_exists($path . "gen/token/$file.php")) {
+         include $path . "gen/token/$file.php";
+      }
    }
-   else if (file_exists(dirname(__FILE__) . "/gen/$file.php")) {
-      include dirname(__FILE__) . "/gen/$file.php";
-   }
-   else if (file_exists(dirname(__FILE__) . "/tree/$file.php")) {
-      include dirname(__FILE__) . "/tree/$file.php";
-   }
-   else if (file_exists(dirname(__FILE__) . "/gen/token/$file.php")) {
-      include dirname(__FILE__) . "/gen/token/$file.php";
-   }
+   spl_autoload_register('__csspurify_autoload');
 }
-spl_autoload_register('__csspurify_autoload');
+else {
+   $path = dirname(__FILE__) . '/';
+   include $path . "CssPurify.php";
+   include $path . "gen/Scanner.php";
+   include $path . "gen/Lexer.php";
+   include $path . "gen/token/Tokenable.php";
+   include $path . "gen/token/Comment.php";
+   include $path . "gen/token/EndRule.php";
+   include $path . "gen/token/EndRules.php";
+   include $path . "gen/token/StartRule.php";
+   include $path . "gen/token/StartRules.php";
+   include $path . "gen/token/Value.php";
+   include $path . "tree/Tree.php";
+}
 ?>
